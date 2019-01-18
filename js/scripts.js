23a4e1a8fe95c25c881321caecba66e4087cd50c
Riley Benson and Travis Wainwright <travis@>
2019-01-17 16:50:19 -0800

    fix jscripts, made form function properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
-    var side1 = $(".side1");
-    var side2 = $(".side2");
-    var side3 = $(".side3");
+    var side1 = $(".side1").val();
+    var side2 = $(".side2").val();
+    var side3 = $(".side3").val();
 
-    if ((side1 === side2) && (side2 === side3)) {
+    if (side1 === side2 && side2 === side3 && side1 === side3) {
       $("#equilateral").show();
     }
     if (side1 === side2)
